e5095fb5594f3bbcef23093392b8b7f50c61e1d2
ISCS-iOS <iosmobile@iscs.com.cn>
2016-04-13 11:20:55 +0800

    add 横屏导航控制器  拥有状态条

---
+++ SAUIKitClass/SALandscapeNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  SALandscapeNavigationController.h
+//  SAUIKit
+//
+//  Created by ISCS01 on 16/4/11.
+//  Copyright © 2016年 浙江网仓科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SALandscapeNavigationController : UINavigationController
+
+@end
